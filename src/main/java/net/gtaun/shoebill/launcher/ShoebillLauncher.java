13262176f30adf9dfc74dc9115f2afc4bc895f15
mk124 <mk124a@gmail.com>
2012-11-21 01:39:31 +0800

    Now resolveDependencies() return a Map container to hold properties.
    Signed-off-by: mk124 <mk124a@gmail.com>

--- src/main/java/net/gtaun/shoebill/launcher/ShoebillLauncher.java
+++ src/main/java/net/gtaun/shoebill/launcher/ShoebillLauncher.java
@@ -10,9 +10,12 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class ShoebillLauncher
 {
+	private static final String PROPERTY_JAR_FILES = "jarFiles";
+	
 	private static final String SHOEBILL_PATH = "shoebill/";
 	private static final String BOOTSTRAP_FOLDER_NAME = "bootstrap/";
 	
@@ -29,37 +32,44 @@ public class ShoebillLauncher
 	};
 	
 	
-	@SuppressWarnings("unchecked")
-	public static List<File> resolveDependencies() throws Throwable
+	public static Object resolveDependencies() throws Throwable
 	{
-		File folder = new File(SHOEBILL_PATH + BOOTSTRAP_FOLDER_NAME);
+		File folder = new File(SHOEBILL_PATH, BOOTSTRAP_FOLDER_NAME);
 		ClassLoader classLoader = createUrlClassLoader(folder.listFiles(JAR_FILENAME_FILTER), null);
 		Class<?> clz = classLoader.loadClass(DEPENDENCY_MANAGER_CLASS_NAME);
 		Method method = clz.getMethod("resolveDependencies");
 		
-		List<File> files;
-		
 		try
 		{
-			files = (List<File>) method.invoke(null);
+			return method.invoke(null);
 		}
 		catch (InvocationTargetException e)
 		{
 			throw e.getTargetException();
 		}
-		
-		return files;
 	}
-	
-	public static Object createShoebill(List<File> files) throws Throwable
+
+	@SuppressWarnings("unchecked")
+	public static Object createShoebill(Object context) throws Throwable
 	{
+		Map<String, Object> properties = Map.class.cast(context);
+		List<File> files = List.class.cast(properties.get(PROPERTY_JAR_FILES));
+		
 		File[] fileArray = new File[files.size()];
 		files.toArray(fileArray);
 		
 		ClassLoader classLoader = createUrlClassLoader(fileArray, null);
 		Class<?> clz = classLoader.loadClass(SHOEBILL_IMPL_CLASS_NAME);
 		Constructor<?> constructor = clz.getConstructor();
-		return constructor.newInstance();
+		
+		try
+		{
+			return constructor.newInstance();
+		}
+		catch (InvocationTargetException e)
+		{
+			throw e.getTargetException();
+		}
 	}
 	
 	private static URLClassLoader createUrlClassLoader(File[] files, ClassLoader parent)
